8a1200b3a65d5d5c59bc701ae243a27f7fdc4f3f
Josh <jrsmith9822@gmail.com>
2015-12-09 23:27:07 +0000

    Main class created with basic functionality

---
+++ main.hs
@@ -0,0 +1,21 @@
+module Main where
+import Utils
+
+main = do
+	content <- getContents
+	print $ unlines $ convertToRST $ lines content
+
+convertToRST :: [[Char]] -> [[Char]]
+convertToRST = parseLines . map trim
+	where
+	parseLines :: [[Char]] -> [[Char]]
+	-- Base case
+	parseLines [] = []
+	-- Title
+	parseLines (('>':cs):css) = (listOf '=' len) : (trim cs) : (listOf '=' len) : parseLines css
+		where len = length cs
+	-- Header
+	parseLines (('=':'>':cs):css) = (listOf '-' len) : (trim cs) : (listOf '-' len) : parseLines css
+		where len = length cs
+	--default
+	parseLines (cs:css) = cs : parseLines css
